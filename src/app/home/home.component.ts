ab3dcc6c8d8129ad8b67a6636ce01a0bf6be330f
Pedro Tauzen <pedrotauzen@gmail.com>
2024-06-01 08:01:16 +0100

    Completed the header.component.html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {HeaderComponent} from "../header/header.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    HeaderComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
